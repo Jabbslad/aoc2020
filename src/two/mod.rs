8ddc970d331bf83e91a9229f2e7d4597e44cef88
Jamie Atkinson <jabbslad@gmail.com>
2020-12-04 03:57:53 +0000

    finished exercise 2

--- src/two/mod.rs
+++ src/two/mod.rs
@@ -1,1 +1,26 @@
-pub fn main() {}
+use crate::util;
+use std::collections::HashSet;
+
+/**
+ * ThreeSum
+*/
+pub fn main() {
+    let nums = util::nums();
+    let mut seen = HashSet::new();
+    for x in 0..nums.len() {
+        for y in (x + 1)..nums.len() {
+            let z = 2020 - nums[x] as i32 - nums[y] as i32;
+            if seen.contains(&z) {
+                return println!(
+                    "{} * {} * {} = {}",
+                    nums[x],
+                    nums[y],
+                    z,
+                    nums[x] * nums[y] * z
+                );
+            } else {
+                seen.insert(nums[y] as i32);
+            }
+        }
+    }
+}
